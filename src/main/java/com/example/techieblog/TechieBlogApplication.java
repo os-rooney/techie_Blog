9928893e12658419285685f1e4af3183c39af3f9
Rooney H. Osman <dev@ronios.de>
2022-04-14 16:14:29 +0200

    upload code

---
+++ src/main/java/com/example/techieblog/TechieBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.techieblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechieBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechieBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techieblog/TechieBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techieblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechieBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
