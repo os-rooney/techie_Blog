831bb1fa24c62999f4993d939c89f5afb117b0ae
Rooney H. Osman <dev@ronios.de>
2022-04-14 16:56:02 +0200

    upload HomeController and MessageRepository

---
+++ src/main/java/com/example/techieblog/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.techieblog;
+
+import com.example.techieblog.message.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class HomeController {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public HomeController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @GetMapping("/")
+    public String home(@ModelAttribute("sessionUser") User sessionUser, Model model) {
+        model.addAttribute("messages", messageRepository.findAllByOrderByPostedAtDesc());
+        return "home";
+    }
+
+}
---
+++ src/main/java/com/example/techieblog/message/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.example.techieblog.message;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findAllByOrderByPostedAtDesc();
+}
