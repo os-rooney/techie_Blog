9e286111c63226ebb2cd8d363fbc280a90925d9b
Anja Schiebel <schiebel955@gmail.com>
2022-04-14 16:48:58 +0200

    add MessageDTO and adjust other classes

---
+++ src/main/java/com/example/techieblog/message/MessageDTO.java
@@ -0,0 +1,35 @@
+package com.example.techieblog.message;
+
+import javax.validation.constraints.Size;
+
+public class MessageDTO {
+
+    @Size(min = 1, max = 99)
+    private String title;
+
+    @Size(min = 1, max = 99)
+    private String description;
+
+    @Size(min = 1, max = 99)
+    private String content;
+
+
+    public MessageDTO(String title, String description, String content) {
+
+        this.description = description;
+        this.content = content;
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
