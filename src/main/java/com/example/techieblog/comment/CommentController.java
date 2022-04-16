4897e9a8108cbc62d98c47968bb04d4f5f70306d
Anja Schiebel <schiebel955@gmail.com>
2022-04-16 10:06:54 +0200

    CommentController update

--- src/main/java/com/example/techieblog/comment/CommentController.java
+++ src/main/java/com/example/techieblog/comment/CommentController.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class CommentController {
 
+    private CommentRepository commentRepository;
+
+    @Autowired
+    public CommentController(CommentRepository commentRepository){
+        this.commentRepository = commentRepository;
+    }
+
     @GetMapping("/article/{messageID}")
     public String article(@PathVariable long messageID, Model model){
+        model.addAttribute("comment", new CommentDTO(""));
         return "article";
     }
 
